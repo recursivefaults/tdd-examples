1ff6ece9fef568c8877910ab77d878b6e1a9ed2b
Ryan Latta <recursive.faults@gmail.com>
2024-05-08 15:41:27 -0500

    Failing test

--- spec/coordinator/coordinator_spec.rb
+++ spec/coordinator/coordinator_spec.rb
@@ -1,3 +1,10 @@
 
 describe "An example with a class that only coordinates" do
+  it "should have a class that calls a coordination method" do
+    worker = SomeWorker.new
+    did_work = worker.do_work
+    
+    expect(did_work).to be_true
+  end
+  
 end
