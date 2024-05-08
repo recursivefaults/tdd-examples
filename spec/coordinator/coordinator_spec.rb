117cbf6f47fe35ddd72d93770c3a6d3048d3ebbe
Ryan Latta <recursive.faults@gmail.com>
2024-05-08 15:51:08 -0500

    removing tests that aren't useful anymore

--- spec/coordinator/coordinator_spec.rb
+++ spec/coordinator/coordinator_spec.rb
@@ -29,25 +29,15 @@ class SomeWorker
   def do_work
     @coordinator.coordinate()
     @logger.log()
-    true
   end
 end
 
 describe "An example with a class that only coordinates" do
-  it "should have a class that calls a coordination method" do
-    some_coordinator = CoordinatorA.new
-    some_logger = Logger.new
-    worker = SomeWorker.new(some_coordinator, some_logger)
-    did_work = worker.do_work
-    expect(did_work).to eq(true)
-  end
-
   it "should use other objects to coordinate" do
     some_coordinator = CoordinatorA.new
     some_logger = Logger.new
     worker = SomeWorker.new(some_coordinator, some_logger)
     did_work = worker.do_work
-    expect(did_work).to eq(true)
     expect(some_coordinator.was_called).to eq(true)
   end
 
@@ -56,8 +46,6 @@ describe "An example with a class that only coordinates" do
     some_logger = Logger.new
     worker = SomeWorker.new(some_coordinator, some_logger)
     did_work = worker.do_work
-    expect(did_work).to eq(true)
-    expect(some_coordinator.was_called).to eq(true)
     expect(some_logger.was_called).to eq(true)
   end
   
