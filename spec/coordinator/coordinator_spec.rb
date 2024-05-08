165e869ac17c5cba127b0bd3c07d5d29f0252b46
Ryan Latta <recursive.faults@gmail.com>
2024-05-08 15:47:45 -0500

    Big commit but putting a first coordinator in. Many small steps here

--- spec/coordinator/coordinator_spec.rb
+++ spec/coordinator/coordinator_spec.rb
@@ -1,12 +1,29 @@
+class CoordinatorA
+  attr_reader :was_called
+  def initialize
+    @was_called = false
+  end
+
+  def coordinate
+    @was_called = true
+  end
+end
+
 class SomeWorker
+  def initialize(coordinator)
+    @coordinator = coordinator
+  end
+
   def do_work
+    @coordinator.coordinate()
     true
   end
 end
 
 describe "An example with a class that only coordinates" do
   it "should have a class that calls a coordination method" do
-    worker = SomeWorker.new
+    some_coordinator = CoordinatorA.new
+    worker = SomeWorker.new(some_coordinator)
     did_work = worker.do_work
     expect(did_work).to eq(true)
   end
