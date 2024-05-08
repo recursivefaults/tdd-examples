d2afc50a971fde2208be0f7e33c93b925d64cf57
Ryan Latta <recursive.faults@gmail.com>
2024-05-08 15:43:41 -0500

    failing test creating some coordinators

--- spec/coordinator/coordinator_spec.rb
+++ spec/coordinator/coordinator_spec.rb
@@ -8,8 +8,15 @@ describe "An example with a class that only coordinates" do
   it "should have a class that calls a coordination method" do
     worker = SomeWorker.new
     did_work = worker.do_work
-    
     expect(did_work).to eq(true)
+  end
+
+  it "should use other objects to coordinate" do
+    some_coordinator = CoordinatorA.new
+    worker = SomeWorker.new(some_coordinator)
+    did_work = worker.do_work
+    expect(did_work).to eq(true)
+    expect(some_coordinator.was_called).to eq(true)
   end
   
 end
