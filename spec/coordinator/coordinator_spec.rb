0aecffcdd69f1b95d705cd6d64227a5387b4ccb1
Ryan Latta <recursive.faults@gmail.com>
2024-05-08 16:01:57 -0500

    Example for methods that only coordinate is finished

--- spec/coordinator/coordinator_spec.rb
+++ spec/coordinator/coordinator_spec.rb
@@ -12,6 +12,17 @@ class Orchestrator
   end
 end
 
+class TestOrchestrator < Orchestrator
+  attr_reader :was_called
+  def initialize
+    @was_called = false
+  end
+
+  def orchestrate
+    @was_called = true
+  end
+end
+
 class Coordinator
   def coordinate
     raise "Not Implemented"
@@ -53,22 +64,20 @@ end
 describe "An example with a class that only coordinates" do
 
   it "should use other objects to coordinate" do
-    some_coordinator = CoordinatorA.new
-    some_logger = Logger.new
-    orchestrator = Orchestrator.new(some_coordinator, some_logger)
+    orchestrator = TestOrchestrator.new
     worker = SomeWorker.new(orchestrator)
     did_work = worker.do_work
-    expect(some_coordinator.was_called).to eq(true)
+    expect(orchestrator.was_called).to eq(true)
   end
-
-  it "should log things when it does work" do
-    some_coordinator = CoordinatorA.new
-    some_logger = Logger.new
-    orchestrator = Orchestrator.new(some_coordinator, some_logger)
-    worker = SomeWorker.new(orchestrator)
-    did_work = worker.do_work
-    expect(some_logger.was_called).to eq(true)
-  end
-  
 end
 
+describe "Orchestrator" do
+  it "coordinates across its components" do
+    some_coordinator = CoordinatorA.new
+    some_logger = Logger.new
+    orchestrator = Orchestrator.new(some_coordinator, some_logger)
+    orchestrator.orchestrate()
+    expect(some_coordinator.was_called).to eq(true)
+    expect(some_logger.was_called).to eq(true)
+  end
+end
